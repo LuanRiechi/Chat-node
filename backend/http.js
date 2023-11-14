ea7d365fba38e587aaf4924680178c6c23ebc8dc
LuanRiechi <luanriechi@alunos.utfpr.edu.br>
2023-11-13 21:04:21 -0300

    Colocando o backend dentro de um container docker

--- backend/http.js
+++ backend/http.js
@@ -5,7 +5,10 @@ const { Server } = require("socket.io");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, 'frontend')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+  });
 
 const serverHttp = http.createServer(app);
 
